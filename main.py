cdb78696b927ff5ec0ef9b11e5f465e69c58445c
mic <micmor.m@gmail.com>
2021-05-13 23:18:43 -0400

    Initial commit

---
+++ main.py
@@ -0,0 +1,15 @@
+alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+
+direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
+text = input("Type your message:\n").lower()
+shift = int(input("Type the shift number:\n"))
+
+
+def encrypt(text_p, shift_p):
+    encrypt_str = ""
+    for ch in text_p:
+        encrypt_str += alphabet[shift_p + alphabet.index(ch)]
+    print(f"The encoded text is {encrypt_str}")
+
+
+encrypt(text, shift)
